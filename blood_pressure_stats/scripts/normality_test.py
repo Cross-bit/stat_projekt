d98328064b35173ba2a488471355152291e4c49c
Cross-bit <ondra.kryz@gmail.com>
2023-09-14 18:21:41 +0200

    QQ plot for blood pressures

--- blood_pressure_stats/scripts/normality_test.py
+++ blood_pressure_stats/scripts/normality_test.py
@@ -4,7 +4,9 @@ from scipy import stats
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import pingouin as pg
 from scipy.stats import norm
+
 matplotlib.use("WebAgg")
 
 hearth_pressure = PersonalHearthPressureData();
@@ -14,27 +16,8 @@ diastolic_pressuers = hearth_pressure.get_dyastolic_pressures(True);
 
 distribution = stats.norm
 
-data = systolic_pressuers
+data = diastolic_pressuers
 
-num_resamples = 500
-
-# Create an array to store bootstrap sample means
-bootstrap_means = []
-
-# Perform bootstrapping
-for _ in range(num_resamples):
-    # Randomly sample with replacement from the original data
-    bootstrap_sample = np.random.choice(data, size=len(data), replace=True)
-    
-    # Calculate the mean of the bootstrap sample
-    bootstrap_mean = np.mean(bootstrap_sample)
-    
-    # Store the bootstrap sample mean
-    bootstrap_means.append(bootstrap_mean)
-
-
-
-#ks_statistic, p_value = stats.kstest(bootstrap_means, distribution.cdf)
 statistic, p_value = stats.shapiro(systolic_pressuers)
 print("systolic: ");
 print(p_value);
@@ -43,23 +26,27 @@ print("diastolic: ");
 print(p_value);
 
 
-data = systolic_pressuers
+data = diastolic_pressuers
 
 std_dev = np.std(data)
 mean = np.mean(data)
 
-
-
-#plt.hist(diastolic_pressuers, bins=40, edgecolor='k')
 print(data)
 
-x = np.linspace(np.min(data), np.max(data), 100)
+x = np.linspace(np.min(data), np.max(data), 1000)
 pdf = norm.pdf(x, mean, std_dev)
 plt.hist(data, edgecolor='k', density=True, alpha=0.5, bins=25)
 
 plt.xlabel('Value')
 plt.ylabel('Frequency')
 plt.title('Histogram of Data')
+
+
+
+plt.figure(figsize=(8, 6))
+#pg.qqplot(data, dist='norm', confidence=.95)
+quantiles, values = stats.probplot(data, dist='norm', plot=plt)
+plt.title("Q-Q Plot for Normal Distribution")
 
 plt.show()
 
